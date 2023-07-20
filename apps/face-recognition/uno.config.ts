4f0661ee2881afcc82bb8a92d1112f953cdd611c
qiuqfang <qiuqfang@outlook.com>
2023-07-20 19:33:04 +0800

    chore: update

--- apps/face-recognition/uno.config.ts
+++ apps/face-recognition/uno.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, presetIcons, presetUno } from 'unocss'
 
 export default defineConfig({
   // ...UnoCSS options
-  presets: [presetUno(), presetIcons({ autoInstall: true })],
+  presets: [presetUno(), presetIcons()],
   shortcuts: {
     btn: 'rounded-lg px-4 py-2 bg-blue-500 text-white border-none cursor-pointer hover:bg-blue-400',
   }
